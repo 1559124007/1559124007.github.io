f6597501e8cf618be778bf4c1eb3b75ddb10f14b
1559124007 <1559124007@qq.com>
2017-01-03 14:29:23 +0800

    mogujieshop

---
+++ mogujie/js/removeNode.js
@@ -0,0 +1,12 @@
+function removeNode(obj,now,clickElem){
+	obj[now].parentNode.removeChild(obj[now]);
+	//获取当前需要删除的元素的下标，然后把后面元素的下标都减1
+	for(var i=now+1;i<clickElem.length+1;i++){
+		clickElem[i-1].index=i-1;
+	}
+}
+function changeIndex(now){
+	for(var i=now+1;i<clickElem.length+1;i++){
+		clickElem[i-1].index=i-1;
+	}
+}
